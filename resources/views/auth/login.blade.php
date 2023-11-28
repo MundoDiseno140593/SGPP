93363ddfaf75d94b30a6decf8a2151c95680d555
MundoDiseno140593 <zeaselvis7@gmail.com>
2023-11-28 16:52:28 -0600

    mod

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,9 @@
 <html lang="en">
 
 <head>
-  <title>Title</title>
+  <title>Login</title>
+  <link rel="icon" href="{{ asset('img/logo.webp') }}" type="image/x-icon"/>
+    <link rel="shortcut icon" href="{{ asset('img/logo.webp') }}" type="image/x-icon"/>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -19,91 +21,62 @@
         <div class="container-fluid h-custom">
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-9 col-lg-6 col-xl-5">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                class="img-fluid" alt="Sample image">
+              <img src="{{ asset('img/logo.webp') }}"
+                class="img-fluid w-70 h-70" alt="Sample image" >
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
               <form>
                 <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                  <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                  </button>
+                  <p class="lead fw-normal mb-0 me-3 "></p>
 
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                  </button>
-
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-linkedin-in"></i>
-                  </button>
                 </div>
 
                 <div class="divider d-flex align-items-center my-4">
-                  <p class="text-center fw-bold mx-3 mb-0">Or</p>
+                  <h2 class="text-center fw-bold mx-3 mb-0 me-3">Iniciar Sesion</h2>
                 </div>
 
                 <!-- Email input -->
                 <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3" class="form-control form-control-lg"
-                    placeholder="Enter a valid email address" />
-                  <label class="form-label" for="form3Example3">Email address</label>
+                  <input type="text" id="user" name="user" class="form-control form-control-lg"
+                    placeholder="Ingrese usuario" />
+                  <label class="form-label" for="form3Example3">usuario:</label>
                 </div>
 
                 <!-- Password input -->
                 <div class="form-outline mb-3">
-                  <input type="password" id="form3Example4" class="form-control form-control-lg"
-                    placeholder="Enter password" />
-                  <label class="form-label" for="form3Example4">Password</label>
+                  <input type="password" id="pass" name="pass" class="form-control form-control-lg"
+                    placeholder="Ingrese Contraseña" />
+                  <label class="form-label" for="form3Example4">Contraseña:</label>
                 </div>
 
-                <div class="d-flex justify-content-between align-items-center">
-                  <!-- Checkbox -->
-                  <div class="form-check mb-0">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                    <label class="form-check-label" for="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="#!" class="text-body">Forgot password?</a>
-                </div>
+
 
                 <div class="text-center text-lg-start mt-4 pt-2">
                   <button type="button" class="btn btn-primary btn-lg"
-                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                  <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                      class="link-danger">Register</a></p>
+                    style="padding-left: 2.5rem; padding-right: 2.5rem;">
+                    Acceso
+                </button>
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <!-- Checkbox -->
+                    <div class="form-check mb-0">
+
+                      <label class="form-check-label" for="form2Example3">
+
+                      </label>
+                    </div>
+                    <a href="#!" class="text-body">Recupera Contraseña</a>
+                  </div>
+
+                  <p class="small fw-bold mt-2 pt-1 mb-0">Si no tienes una cuenta? <a href="#!"
+                      class="link-danger">Registrarte</a></p>
                 </div>
 
               </form>
             </div>
           </div>
         </div>
-        <div
-          class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-          <!-- Copyright -->
-          <div class="text-white mb-3 mb-md-0">
-            Copyright © 2023. Desarrollado Por Ing.Diana Ramos && Ing.Elvis Pavon.
-          </div>
-          <!-- Copyright -->
 
-          <!-- Right -->
-          <div>
-            <a href="#!" class="text-white me-4">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="#!" class="text-white me-4">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#!" class="text-white me-4">
-              <i class="fab fa-google"></i>
-            </a>
-            <a href="#!" class="text-white">
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-          <!-- Right -->
-        </div>
       </section>
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
